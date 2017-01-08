5ca6ea4d6621475eee7eb9f7cb25e2afc4858313
unknown <sakai.jun01@gmail.com>
2017-01-08 18:50:13 +0100

    bt updated

--- mvc/binaryTable.js
+++ mvc/binaryTable.js
@@ -1,24 +1,38 @@
 class BinaryTable {
 
     dec2Bin(dec) {
-        let str = "";
+
         let binArr = [];
-        //unicode for exponents 0...6        
-        let utfExp = ["\u2070", "\u00B9", "\u00B2", "\u00B3", "\u2074", "\u2075", "\u2076"];
-        
+        let year64 = 0;
+
         //dec to bin conversions
         for (let i = 0; i < dec.length; i++) {
-            binArr[i] = ("000000" + parseInt(dec[i]).toString(2)).slice(-7);
+            //if year >=64 append 1 additional col
+            if (parseInt(dec[dec.length - 1]) >= 64) {
+                binArr[i] = ("000000" + parseInt(dec[i]).toString(2)).slice(-7);
+                year64 = 1;
+            } else {
+                binArr[i] = ("00000" + parseInt(dec[i]).toString(2)).slice(-6);
+                year64 = 0;
+            }
         }
+        return this.htmlTable(binArr, dec, year64);
+    }
+
+
+    htmlTable(binArr, dec, year64) {
+        //unicode for exponents 0...6       
+        let utfExp = ["\u2070", "\u00B9", "\u00B2", "\u00B3", "\u2074", "\u2075", "\u2076"];
+        let str = "";
+
         //first row for powers of 2
         str += "<tr>";
-        for (let i = binArr.length + 1; i >= 0; i--) {
-            (i === binArr.length + 1)
+        for (let i = binArr.length + year64; i >= 0; i--) {
+            (i === binArr.length + year64)
                 ? str += `<th id="btmRight"></th>`
                 : str += `<th id="bottom">2${utfExp[i]}</th>`;
         }
         str += "</tr>";
-        
         //rows for binary numbers of each time unit
         for (let i = 0; i < binArr.length; i++) {
             str += "<tr>";
